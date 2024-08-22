9578a6eff37eea602975aa8e628755a9b7a9782c
WEI HAN CHEN <weihanchen0725@gmail.com>
2024-08-22 00:21:30 -0700

    edit settings

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://weihanchen0725.github.io/movie-list/",
+  base: "/movie-list",
   build: {
     outDir: "dist",
   },
